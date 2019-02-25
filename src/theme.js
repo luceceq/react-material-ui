059cc8773d7db591b1d66757d34162eb1fc6487c
lucenildo <dev.luce84@outlook.com>
2019-02-24 23:02:04 -0300

    accent color change

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,13 @@
-import { createMuiTheme } from 'material-ui/styles';
+import {createMuiTheme} from 'material-ui/styles';
 import indigo from 'material-ui/colors/purple';
 import orange from 'material-ui/colors/purple';
+import {red100} from 'material-ui/styles/colors';
 
 // Configure Material UI theme
-const theme = createMuiTheme({
+const theme = createMuiTheme ({
   palette: {
     primary: indigo,
-    accent: orange,
+    accent: red100,
     type: 'light',
   },
 });
