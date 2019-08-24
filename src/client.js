f50b4e286353d25feb0af9c4458a88bc884c4040
luceceq <dev.luce84@outlook.com>
2019-08-24 00:28:57 -0300

    qweqweqwe

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import theme from './theme';
-import { MuiThemeProvider } from 'material-ui/styles';
-import App from './App';
+import React from "react";
+import { hydrate } from "react-dom";
+import theme from "./theme";
+import { MuiThemeProvider } from "material-ui/styles";
+import App from "./App";
 
 // This is needed in order to deduplicate the injection of CSS in the page.
 const sheetsManager = new WeakMap();
@@ -10,14 +10,7 @@ const sheetsManager = new WeakMap();
 hydrate(
   <MuiThemeProvider sheetsManager={sheetsManager} theme={theme}>
     <App />
-  </MuiThemeProvider>,
-  document.getElementById('root'),
-  () => {
-    // [ReHydratation](https://github.com/cssinjs/jss/blob/master/docs/ssr.md)
-    const jssStyles = document.getElementById('jss-ssr');
-    if (jssStyles && jssStyles.parentNode)
-      jssStyles.parentNode.removeChild(jssStyles);
-  }
+  </MuiThemeProvider>
 );
 
 if (module.hot) {
